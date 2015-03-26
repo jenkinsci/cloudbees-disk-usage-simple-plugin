750e3e0b12e46cfa83ecdf11d0ea0b18282adff5
Nicolas De Loof <nicolas.deloof@gmail.com>
2015-03-27 00:02:43 +0100

    code review

--- src/main/java/com/cloudbees/simplediskusage/QuickDiskUsage.java
+++ src/main/java/com/cloudbees/simplediskusage/QuickDiskUsage.java
@@ -1,19 +1,21 @@
 package com.cloudbees.simplediskusage;
 
 import hudson.Extension;
+import hudson.Util;
 import hudson.model.*;
 import hudson.security.ACL;
 import hudson.security.Permission;
 import jenkins.model.Jenkins;
+import jenkins.util.Timer;
 import org.acegisecurity.context.SecurityContext;
 import org.acegisecurity.context.SecurityContextHolder;
 import org.kohsuke.stapler.*;
+import org.kohsuke.stapler.interceptor.RequirePOST;
 
 import javax.inject.Singleton;
 import javax.servlet.ServletException;
 import java.io.*;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -45,91 +47,78 @@ public class QuickDiskUsage extends ManagementLink {
         }
     });
 
-    Map<Job, Long> usage = new HashMap<Job, Long>();
+    Map<DiskItem, Long> usage = new HashMap<>();
     long lastRun = 0;
 
-    public Map<Job, Long> getDiskUsage() throws IOException {
-        fetchUsage(Jenkins.getInstance().getRootDir());
-        Map<Job, Long> sorted =  new TreeMap<Job, Long>(BY_NAME);
+    public Map<DiskItem, Long> getDiskUsage() throws IOException {
+        fetchUsage();
+        Map<DiskItem, Long> sorted =  new TreeMap<>(BY_NAME);
         sorted.putAll(usage);
         return sorted;
     }
 
-    private static Comparator<Job> BY_NAME = new Comparator<Job>() {
+    private static Comparator<DiskItem> BY_NAME = new Comparator<DiskItem>() {
         @Override
-        public int compare(Job o1, Job o2) {
+        public int compare(DiskItem o1, DiskItem o2) {
             return o1.getFullName().compareTo(o2.getFullName());
         }
     };
-
-
 
     public long getLastRun() {
         return lastRun;
     }
 
-    public Date getLastRunDate() {
-        return new Date(lastRun);
+    public String getSince() {
+        return Util.getPastTimeString(System.currentTimeMillis() - lastRun);
     }
 
+    @RequirePOST
     public void doRefresh(StaplerRequest req, StaplerResponse res) throws IOException, ServletException {
         lastRun = 0;
         usage.clear();
-        fetchUsage(Jenkins.getInstance().getRootDir());
+        fetchUsage();
         res.forwardToPreviousPage(req);
     }
 
-    public void doClean(StaplerRequest req, StaplerResponse res) throws IOException, ServletException, InterruptedException {
-        Job job = Jenkins.getInstance().getItemByFullName(req.getParameter("job"), Job.class);
-        job.logRotate();
+    @RequirePOST
+    public void doClean(StaplerRequest req, StaplerResponse res) throws IOException, ServletException {
+        final Job job = Jenkins.getInstance().getItemByFullName(req.getParameter("job"), Job.class);
+        Timer.get().submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    job.logRotate();
+                } catch (Exception e) {
+                    logger.log(Level.WARNING, "logRotate failed", e);
+                }
+            }
+        });
         res.forwardToPreviousPage(req);
     }
-
 
     @Override
     public Permission getRequiredPermission() {
         return Jenkins.ADMINISTER;
     }
 
-    private void fetchUsage(final File rootDir) throws IOException {
-        final QuickDiskUsage self = this;
+    private void fetchUsage() throws IOException {
         if (System.currentTimeMillis() - lastRun < 3 * 60 * 1000) {
             return;
         }
         logger.info("Re-estimating disk usage");
-        ex.execute(new Runnable() {
-            public void run() {
-                Map<Job, Long> usage = new HashMap<Job, Long>();
-                SecurityContext impersonate = ACL.impersonate(ACL.SYSTEM);
-                try {
-                    for (Job item : Jenkins.getInstance().getAllItems(Job.class)) {
-                        usage.put(item, duJob(item));
-                    }
-                    logger.info("Finished re-estimating disk usage.");
-                    QuickDiskUsage.this.usage = usage;
-
-                } catch (Exception e) {
-                    logger.log(Level.INFO, "Unable to run disk usage check", e);
-                } finally {
-                    SecurityContextHolder.setContext(impersonate);
-                }
-                lastRun = System.currentTimeMillis();
-            }
-        });
-
+        ex.execute(computeDiskUsage);
     }
 
     private long duJob(Job item) throws IOException, InterruptedException {
         logger.info("Estimating usage for: " + item.getDisplayName());
         Process p = Runtime.getRuntime().exec(DISK_USAGE, null, item.getRootDir());
         StringBuilder du = new StringBuilder();
-        BufferedReader stdOut = new BufferedReader (new InputStreamReader(p.getInputStream()));
-        String line = stdOut.readLine();
-
-        Thread.sleep(1000); //To keep load average nice and low
-        if (line.matches("[0-9]*\t.")) return Long.parseLong(line.substring(0, line.length() -2));
-        logger.log(Level.WARNING, "failed to parse `du` output : "+line);
-        return -1;
+        try (BufferedReader stdOut = new BufferedReader (new InputStreamReader(p.getInputStream())) ) {
+            String line = stdOut.readLine();
+            if (line.matches("[0-9]*\t.")) return Long.parseLong(line.substring(0, line.length() -2));
+            logger.log(Level.WARNING, "failed to parse `du` output : "+line);
+            return -1;
+        }
     }
 
 
@@ -152,4 +141,26 @@ public class QuickDiskUsage extends ManagementLink {
 
     private static final Logger logger = Logger.getLogger(QuickDiskUsage.class.getName());
 
+    private final Runnable computeDiskUsage = new Runnable() {
+        public void run() {
+            Map<DiskItem, Long> usage = new HashMap<>();
+            SecurityContext impersonate = ACL.impersonate(ACL.SYSTEM);
+            try {
+                for (Job item : Jenkins.getInstance().getAllItems(Job.class)) {
+                    if (item instanceof TopLevelItem)
+                        usage.put(new DiskItem(item), duJob(item));
+
+                    Thread.sleep(1000); //To keep load average nice and low
+                }
+                logger.info("Finished re-estimating disk usage.");
+                QuickDiskUsage.this.usage = usage;
+
+            } catch (Exception e) {
+                logger.log(Level.INFO, "Unable to run disk usage check", e);
+            } finally {
+                SecurityContextHolder.setContext(impersonate);
+            }
+            lastRun = System.currentTimeMillis();
+        }
+    };
 }
